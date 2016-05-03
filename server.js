a7680f33b8b4ae1690c5c6f3988908da8357cdc0
Roy Hashimoto <roy@shoestringresearch.com>
2016-05-03 12:09:52 -0700

    Set logging to TRACE.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var nxb    = require('node-xmpp-bosh');
 var server = nxb.start_bosh({
-  logging: "WARN",
+  logging: "TRACE",
   host: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
   port: process.env.OPENSHIFT_NODEJS_PORT || 5280, 
   path: /^\/http-bind(\/+)?$/
